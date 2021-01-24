6d9f4b372f705e98717c6d8aebae6a32421f63d2
daneswari-danu <76155114+daneswari-danu@users.noreply.github.com>
2021-01-24 17:06:40 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "this is my file"
-  free -m
-  echo " this is memory usage in my instance"
-  cd /home/danu
-  rm -rf *
-  touch file1
-  ls -la
+cd /home/danu
+ touch danu123
+ ls -la
